d68319cd192ceada13676957729984fe90e1f23d
lucivaldoqueiroz <lucivaldo.npqueiroz@gmail.com>
2022-02-01 01:22:57 -0300

    constraintLayout + escuta de botão Forma 1.

---
+++ app/src/main/java/com/example/beberagua/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.beberagua;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnNotify;
+    private EditText editMinutes;
+    private TimePicker timePicker;
+
+    private int hour;
+    private int minute;
+    private int interval;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    btnNotify = findViewById(R.id.btn_notify);
+    editMinutes = findViewById(R.id.edit_text_number_interval);
+    timePicker = findViewById(R.id.time_picker);
+
+    timePicker.setIs24HourView(true);
+    }
+
+    public void notifyClick(View view){
+
+        String sInterval = editMinutes.getText().toString();
+
+        hour = timePicker.getCurrentHour();
+        minute = timePicker.getCurrentMinute();
+        interval = Integer.parseInt(sInterval);
+
+        Log.d("Teste", "hora: " + hour + " Minuto: " + minute + " intervalo: " + interval);
+    }
+
+}
